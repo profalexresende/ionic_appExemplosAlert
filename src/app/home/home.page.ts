0cc73091ce843b08d433194a45959892a637e691
profalexresende <82842833+profalexresende@users.noreply.github.com>
2022-04-14 07:58:36 -0300

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,348 @@
+/* eslint-disable @typescript-eslint/dot-notation */
+import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(public toastController: ToastController,
+    public alertController: AlertController) {}
+
+  async toastComOpcao() {
+    const toast = await this.toastController.create({
+      header: 'Toast header',
+      message: 'Click to Close',
+      icon: 'information-circle',
+      position: 'bottom',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Botão 1',
+          handler: () => {
+            console.log('Botão 1');
+          }
+        }, {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Botão 2');
+          }
+        }
+      ]
+    });
+    await toast.present();
+
+    const { role } = await toast.onDidDismiss();
+    console.log('Quando o toast fecha, executa o role', role);
+  }
+
+
+  async alertaSimples() {
+    const alert = await this.alertController.create({
+      header: 'Título',
+      subHeader: 'Subtitulo',
+      message: 'Este é o meu alerta.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async alertaMultiplosBotoes() {
+    const alert = await this.alertController.create({
+      header: 'Alerta',
+      subHeader: 'Subtitulo',
+      message: 'Alerta com vários buttons.',
+      buttons: ['Cancelar', 'Abrir', 'Excluir']
+    });
+
+    await alert.present();
+  }
+
+  async alertaConfirmar() {
+    const alert = await this.alertController.create({
+      header: 'Atenção',
+      message: 'Alerta <strong>com confirmação</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          id: 'cancel-button',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        }, {
+          text: 'OK',
+          id: 'confirm-button',
+          handler: () => {
+            console.log('OK');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async alertaEntrada() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Entrada Texto',
+        },
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          value: 'Com valor padrão',
+          placeholder: 'Placeholder 2'
+        },
+        // multiline input.
+        {
+          name: 'paragraph',
+          id: 'paragraph',
+          type: 'textarea',
+          placeholder: 'MultLine'
+        },
+        {
+          name: 'name3',
+          value: 'http://ionicframework.com',
+          type: 'url',
+          placeholder: 'Tipo URL'
+        },
+        // input date with min & max
+        {
+          name: 'name4',
+          type: 'date',
+          min: '2017-03-01',
+          max: '2018-01-12'
+        },
+        // input date without min nor max
+        {
+          name: 'name5',
+          type: 'date'
+        },
+        {
+          name: 'name6',
+          type: 'number',
+          min: -5,
+          max: 10
+        },
+        {
+          name: 'name7',
+          type: 'number'
+        },
+        {
+          name: 'name8',
+          id:'teste',
+          type: 'password',
+          placeholder: 'Advanced Attributes',
+          cssClass: 'specialClass',
+          attributes: {
+            maxlength: 4,
+            inputmode: 'decimal'
+          }
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        }, {
+          text: 'Ok',
+          handler: (dado: any) => {
+            console.log(dado['name1']);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+
+  }
+
+  async presentAlertRadio() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Radio',
+      inputs: [
+        {
+          name: 'radio1',
+          type: 'radio',
+          label: 'Radio 1',
+          value: 'value1',
+          handler: () => {
+            console.log('Radio 1 selected');
+          },
+          checked: true
+        },
+        {
+          name: 'radio2',
+          type: 'radio',
+          label: 'Radio 2',
+          value: 'value2',
+          handler: () => {
+            console.log('Radio 2 selected');
+          }
+        },
+        {
+          name: 'radio3',
+          type: 'radio',
+          label: 'Radio 3',
+          value: 'value3',
+          handler: () => {
+            console.log('Radio 3 selected');
+          }
+        },
+        {
+          name: 'radio4',
+          type: 'radio',
+          label: 'Radio 4',
+          value: 'value4',
+          handler: () => {
+            console.log('Radio 4 selected');
+          }
+        },
+        {
+          name: 'radio5',
+          type: 'radio',
+          label: 'Radio 5',
+          value: 'value5',
+          handler: () => {
+            console.log('Radio 5 selected');
+          }
+        },
+        {
+          name: 'radio6',
+          type: 'radio',
+          label: 'Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 Radio 6 ',
+          value: 'value6',
+          handler: () => {
+            console.log('Radio 6 selected');
+          }
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async presentAlertCheckbox() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Checkbox',
+      inputs: [
+        {
+          name: 'checkbox1',
+          type: 'checkbox',
+          label: 'Checkbox 1',
+          value: 'value1',
+          handler: () => {
+            console.log('Checkbox 1 selected');
+          },
+          checked: true
+        },
+
+        {
+          name: 'checkbox2',
+          type: 'checkbox',
+          label: 'Checkbox 2',
+          value: 'value2',
+          handler: () => {
+            console.log('Checkbox 2 selected');
+          }
+        },
+
+        {
+          name: 'checkbox3',
+          type: 'checkbox',
+          label: 'Checkbox 3',
+          value: 'value3',
+          handler: () => {
+            console.log('Checkbox 3 selected');
+          }
+        },
+
+        {
+          name: 'checkbox4',
+          type: 'checkbox',
+          label: 'Checkbox 4',
+          value: 'value4',
+          handler: () => {
+            console.log('Checkbox 4 selected');
+          }
+        },
+
+        {
+          name: 'checkbox5',
+          type: 'checkbox',
+          label: 'Checkbox 5',
+          value: 'value5',
+          handler: () => {
+            console.log('Checkbox 5 selected');
+          }
+        },
+
+        {
+          name: 'checkbox6',
+          type: 'checkbox',
+          label: 'Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6 Checkbox 6',
+          value: 'value6',
+          handler: () => {
+            console.log('Checkbox 6 selected');
+          }
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+}
